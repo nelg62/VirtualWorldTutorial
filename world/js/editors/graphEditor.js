bc2f6f05c79f4c006a6a664acd0b99156283101e
nelg62 <glen.harding.nz@gmail.com>
2025-03-31 16:55:26 +1300

    lesson 7 done

--- world/js/editors/graphEditor.js
+++ world/js/editors/graphEditor.js
@@ -33,13 +33,14 @@ class GraphEditor {
     this.canvas.addEventListener("contextmenu", this.boundContextMenu);
 
     window.addEventListener("keydown", (evt) => {
-      if (this.hovered) {
-        if (evt.key == "s") {
-          this.start = this.hovered;
-        }
-        if (evt.key == "e") {
-          this.end = this.hovered;
-        }
+      if (evt.key == "s") {
+        this.start = this.mouse;
+      }
+      if (evt.key == "e") {
+        this.end = this.mouse;
+      }
+      if (this.start && this.end) {
+        world.generateCorridor(this.start, this.end);
       }
     });
   }
@@ -118,16 +119,6 @@ class GraphEditor {
       const intent = this.hovered ? this.hovered : this.mouse;
       new Segment(this.selected, intent).draw(ctx, { dash: [3, 3] });
       this.selected.draw(this.ctx, { outline: true });
-    }
-
-    if (this.start && this.end) {
-      const path = this.graph.getShortestPath(this.start, this.end);
-      for (const point of path) {
-        point.draw(this.ctx, { size: 50, color: "blue" });
-        if (point.prev) {
-          new Segment(point, point.prev).draw(this.ctx, { width: 20 });
-        }
-      }
     }
   }
 }
